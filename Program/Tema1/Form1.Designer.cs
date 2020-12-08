8e36ddb69797ad2abc2485d0dc4281b64519a060
Nuna Razvan <30498668+zalaw@users.noreply.github.com>
2020-12-08 15:01:57 +0200

    Add files via upload
    Program pentru Tema 1: Desenare linii

---
+++ Program/Tema1/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Tema1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.reset = new System.Windows.Forms.Button();
+            this.thickness = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.y2 = new System.Windows.Forms.TextBox();
+            this.draw = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.algorithm = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thickness)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 118);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(920, 391);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Grosime linie";
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(219, 55);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(147, 37);
+            this.reset.TabIndex = 7;
+            this.reset.Text = "Reseteaza";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // thickness
+            // 
+            this.thickness.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thickness.Location = new System.Drawing.Point(133, 12);
+            this.thickness.Maximum = new decimal(new int[] {
+            64,
+            0,
+            0,
+            0});
+            this.thickness.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.thickness.Name = "thickness";
+            this.thickness.Size = new System.Drawing.Size(62, 22);
+            this.thickness.TabIndex = 1;
+            this.thickness.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "x1";
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(48, 40);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(52, 20);
+            this.x1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(110, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "y1";
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(143, 40);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(52, 20);
+            this.y1.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "x2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(110, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "y2";
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(48, 72);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(52, 20);
+            this.x2.TabIndex = 4;
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(143, 72);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(52, 20);
+            this.y2.TabIndex = 5;
+            // 
+            // draw
+            // 
+            this.draw.Location = new System.Drawing.Point(219, 12);
+            this.draw.Name = "draw";
+            this.draw.Size = new System.Drawing.Size(147, 37);
+            this.draw.TabIndex = 6;
+            this.draw.Text = "Deseneaza";
+            this.draw.UseVisualStyleBackColor = true;
+            this.draw.Click += new System.EventHandler(this.draw_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(372, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "DDA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Blue;
+            this.button2.Location = new System.Drawing.Point(372, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 37);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Bresenham";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // algorithm
+            // 
+            this.algorithm.AutoSize = true;
+            this.algorithm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.algorithm.Location = new System.Drawing.Point(536, 19);
+            this.algorithm.Name = "algorithm";
+            this.algorithm.Size = new System.Drawing.Size(57, 20);
+            this.algorithm.TabIndex = 9;
+            this.algorithm.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(944, 521);
+            this.Controls.Add(this.algorithm);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.draw);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.x1);
+            this.Controls.Add(this.thickness);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Linii";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thickness)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.NumericUpDown thickness;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox x1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox y1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox x2;
+        private System.Windows.Forms.TextBox y2;
+        private System.Windows.Forms.Button draw;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label algorithm;
+    }
+}
+
---
+++ Program/Tema1/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Windows.Markup;
+using System.Xml;
+using System.Globalization;
+
+namespace Tema1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap btm;
+        int pointsLeft = 2;
+        int met = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            algorithm.Text = "DDA";
+            algorithm.ForeColor = Color.Red;
+        }
+
+        private void draw_Click(object sender, EventArgs e)
+        {
+            float xa, ya, xb, yb;
+
+            if (x1.Text == "" || y1.Text == "" || x2.Text == "" || y2.Text == "")
+            {
+                MessageBox.Show("Toate campurile trebuie completate!");
+            }
+            else if (!float.TryParse(x1.Text, out xa) || !float.TryParse(y1.Text, out ya) || !float.TryParse(x2.Text, out xb) || !float.TryParse(y2.Text, out yb))
+            {
+                MessageBox.Show("Coordonatele trebuie sa fie numere reale!");
+            }
+            else if (xa < 0 || xa > pictureBox1.Width || ya < 0 || ya > pictureBox1.Height || xb < 0 || xb > pictureBox1.Width || yb < 0 || yb > pictureBox1.Height)
+            {
+                MessageBox.Show("Coordonatele trebuie sa se afle in interiorul pictureBox-ului!");
+            }
+            else
+            {
+                btm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+                pictureBox1.Image = null;
+
+                if (met == 0)
+                {
+                    DDA(float.Parse(x1.Text), float.Parse(y1.Text), float.Parse(x2.Text), float.Parse(y2.Text));
+                }
+                else
+                {
+                    Bresenham(int.Parse(x1.Text), int.Parse(y1.Text), int.Parse(x2.Text), int.Parse(y2.Text));
+                }
+
+                pictureBox1.Image = btm;
+            }
+            
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            btm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            pictureBox1.Image = null;
+
+            pointsLeft = 2;
+
+            x1.Text = "";
+            y1.Text = "";
+            x2.Text = "";
+            y2.Text = "";
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (pointsLeft == 2)
+            {
+                x1.Text = e.X.ToString();
+                y1.Text = e.Y.ToString();
+
+                pointsLeft--;
+            }
+            else if (pointsLeft == 1)
+            {
+                x2.Text = e.X.ToString();
+                y2.Text = e.Y.ToString();
+                pointsLeft--;
+
+                draw_Click(null, null);
+            }
+            else
+            {
+                reset_Click(null, null);
+
+                x1.Text = e.X.ToString();
+                y1.Text = e.Y.ToString();
+
+                pointsLeft--;
+            }
+        } 
+
+        private void DDA(float x1, float y1, float x2, float y2)
+        {
+            float dx, dy, totalPixels, xInc, yInc;
+
+            dx = x2 - x1;
+            dy = y2 - y1;
+
+            float slope = dy / dx;
+
+            totalPixels = Math.Max(Math.Abs(dx), Math.Abs(dy));
+
+            xInc = dx / totalPixels;
+            yInc = dy / totalPixels;
+
+            for (int i = 0; i < totalPixels; i++)
+            {
+                btm.SetPixel((int)Math.Round(x1), (int)Math.Round(y1), Color.Red);
+
+                // Grosime
+                if (int.Parse(thickness.Text) > 1)
+                {
+                    if (Math.Abs(slope) < 1)
+                    {
+                        float j = (int)y1;
+                        while (j <= y1 + int.Parse(thickness.Text) / 2 && j < pictureBox1.Height)
+                        {
+                            btm.SetPixel((int)Math.Round(x1), (int)Math.Round(j), Color.Red);
+                            j++;
+                        }
+                    }
+                    else
+                    {
+                        float j = (int)x1;
+                        while (j <= x1 + int.Parse(thickness.Text) / 2 && j < pictureBox1.Width)
+                        {
+                            btm.SetPixel((int)Math.Round(j), (int)Math.Round(y1), Color.Red);
+                            j++;
+                        }
+                    }
+                }
+                
+
+                x1 += xInc;
+                y1 += yInc;
+            }
+        }
+
+        private void Bresenham(int x, int y, int x2, int y2)
+        {
+            int w = x2 - x;
+            int h = y2 - y;
+            int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+
+            if (w < 0)
+            {
+                dx1 = -1;
+            }
+            else if (w > 0)
+            {
+                dx1 = 1;
+            }
+
+            if (h < 0)
+            {
+                dy1 = -1;
+            }
+            else if (h > 0)
+            {
+                dy1 = 1;
+            }
+
+            if (w < 0)
+            {
+                dx2 = -1;
+            }
+            else if (w > 0)
+            {
+                dx2 = 1;
+            }
+
+            int longest = Math.Abs(w);
+            int shortest = Math.Abs(h);
+
+            if (!(longest > shortest))
+            {
+                longest = Math.Abs(h);
+                shortest = Math.Abs(w);
+
+                if (h < 0)
+                {
+                    dy2 = -1;
+                }
+                else if (h > 0)
+                {
+                    dy2 = 1;
+                }
+
+                dx2 = 0;
+            }
+
+            float slope = (float)(y2 - y) / (x2 - x);
+
+            int numerator = longest >> 1;
+
+            for (int i = 0; i <= longest; i++)
+            {
+                btm.SetPixel(x, y, Color.Blue);
+                
+                if (Math.Abs(slope) < 1)
+                {
+                    float j = (int)y;
+                    while (j <= y + int.Parse(thickness.Text) / 2 && j < pictureBox1.Height)
+                    {
+                        btm.SetPixel(x, (int)Math.Round(j), Color.Blue);
+                        j++;
+                    }
+                }
+                else
+                {
+                    float j = (int)x;
+                    while (j <= x + int.Parse(thickness.Text) / 2 && j < pictureBox1.Width)
+                    {
+                        btm.SetPixel((int)Math.Round(j), y, Color.Blue);
+                        j++;
+                    }
+                }
+
+                numerator += shortest;
+
+                if (!(numerator < longest))
+                {
+                    numerator -= longest;
+                    x += dx1;
+                    y += dy1;
+                }
+                else
+                {
+                    x += dx2;
+                    y += dy2;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            met = 0;
+
+            algorithm.Text = "DDA";
+            algorithm.ForeColor = Color.Red;
+
+            reset_Click(null, null);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            met = 1;
+
+            algorithm.Text = "Bresenham";
+            algorithm.ForeColor = Color.Blue;
+
+            reset_Click(null, null);
+        }
+    }
+}
